a8bd25f08b44c84fc9735bba839d31a378c47d20
Umashankar Uikey <umashankaruikey325@gmail.com>
2024-11-18 23:05:32 +0530

    finalcommit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-3xl bg-slate-300 text-red'>App</div>
+    <div className='text-3xl bg-slate-300 text-red'>
+      <h1>App</h1>
+      <h2>{import.meta.env.VITE_CROMPASSS}</h2>
+      </div>
   )
 }
 
