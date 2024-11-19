ae58b62681041792db8bbc145c12aee6717914df
Umashankar Uikey <umashankaruikey325@gmail.com>
2024-11-19 19:16:17 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div className='text-3xl bg-slate-300 text-red'>
+    <div className="text-3xl bg-slate-300 text-red">
       <h1>App</h1>
       <h2>{import.meta.env.VITE_CROMPASSS}</h2>
-      </div>
-  )
-}
+      <h2>umashankar</h2>
+    </div>
+  );
+};
 
-export default App  
+export default App;
